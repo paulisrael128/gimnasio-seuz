3803d27be4e54cdea33374b5e283f51674e6684d
paulisrael128 <titancronos.paul@gmail.com>
2016-07-12 15:36:02 -0500

    login
    agregando login y interfaz para gerente

---
+++ validar.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+	require("connect_db.php");
+
+	$username=$_POST['mail'];
+	$pass=$_POST['pass'];
+
+
+
+	$sql2=mysql_query("SELECT * FROM login WHERE email='$username'");
+	if($f2=mysql_fetch_array($sql2)){
+		if($pass==$f2['pasadmin']){
+			$_SESSION['id']=$f2['id'];
+			$_SESSION['user']=$f2['user'];
+			echo '<script>alert("BIENVENIDO ADMINISTRADOR")</script> ';
+			echo "<script>location.href='admin.php'</script>";
+		
+		}
+	
+	}
+
+
+	$sql=mysql_query("SELECT * FROM login WHERE email='$username'");
+	if($f=mysql_fetch_array($sql)){
+		if($pass==$f['password']){
+			$_SESSION['id']=$f['id'];
+			$_SESSION['user']=$f['user'];
+			echo '<script>alert("BIENVENIDO GERENTE")</script> ';
+			echo "<script>location.href='gerente.php'</script>";
+		}else{
+			echo '<script>alert("CONTRASEÑA INCORRECTA")</script> ';
+		
+			echo "<script>location.href='acceso.php'</script>";
+		}
+	}else{
+		
+		echo '<script>alert("ESTE USUARIO NO EXISTE, PORFAVOR REGISTRESE PARA PODER INGRESAR")</script> ';
+		
+		echo "<script>location.href='index.php'</script>";	
+
+	}
+
+?>
